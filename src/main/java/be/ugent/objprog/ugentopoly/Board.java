8dd552a8f060c7e19a0770edd91714e7cb42e3ce
Kaya-Sem <kayasemvc@gmail.com>
2024-03-15 15:19:28 +0100

    updates

--- src/main/java/be/ugent/objprog/ugentopoly/Board.java
+++ src/main/java/be/ugent/objprog/ugentopoly/Board.java
@@ -1,5 +1,8 @@
 package be.ugent.objprog.ugentopoly;
 
+import be.ugent.objprog.ugentopoly.tiles.CornerTile;
+import be.ugent.objprog.ugentopoly.tiles.HorizontalTile;
+import be.ugent.objprog.ugentopoly.tiles.VerticalTile;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
@@ -9,24 +12,44 @@ public class Board extends VBox {
     public static final int BOARD_WIDTH = 845;
 
     public static final int BOARD_HEIGHT = 845;
-    VBox board;
-
-
 
     public Board() {
         setPrefSize(BOARD_WIDTH, BOARD_HEIGHT);
-        setStyle("-fx-background-color: lightblue;");
+        setAlignment(Pos.CENTER);
+        setStyle("-fx-background-color: #119dcb;");
 
         HBox upperRow = new HBox();
         HBox bottomRow = new HBox();
         HBox midSection = new HBox();
+        VBox leftBar = new VBox();
+        VBox rightBar = new VBox();
 
-        for (int i = 0; i < 13; i++) {
-            upperRow.getChildren().add(new Button("button"));
+        // fill upper row
+        upperRow.getChildren().add(new CornerTile());
+        for (int i = 0; i < 11; i++) {
+            upperRow.getChildren().add(new VerticalTile());
+        }
+        upperRow.getChildren().add(new CornerTile());
+
+
+        // fill bottom row
+        bottomRow.getChildren().add(new CornerTile());
+        for (int i = 0; i < 11; i++) {
+            bottomRow.getChildren().add(new VerticalTile());
+        }
+        bottomRow.getChildren().add(new CornerTile());
+
+        for (int i = 0; i < 11; i++) {
+            leftBar.getChildren().add(new HorizontalTile());
+        }
+        for (int i = 0; i < 11; i++) {
+            rightBar.getChildren().add(new HorizontalTile());
         }
 
-        midSection.getChildren().addAll(new VBox(), new HBox(), new VBox() );
+
+        midSection.getChildren().addAll(leftBar, new HBox(), rightBar );
 
         this.getChildren().addAll(upperRow, midSection, bottomRow);
+
     }
 }
--- src/main/java/be/ugent/objprog/ugentopoly/Ugentopoly.java
+++ src/main/java/be/ugent/objprog/ugentopoly/Ugentopoly.java
@@ -2,36 +2,37 @@ package be.ugent.objprog.ugentopoly;
 
 import be.ugent.objprog.dice.Dice;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Ugentopoly extends Application {
-    static final String LOGO = "/be/ugent/objprog/ugentopoly/assets/tax.png";
+    static final String ICON = "tax.png";
     @Override
     public void start(Stage stage) {
-
-
 
         final Dice dice = new Dice();
         //Color backgroundColor = Color.rgb(0,  255, 202);
 
-        StackPane root = new StackPane();
+        StackPane root = new StackPane(new Board());
 
 
 
 
+        Scene scene = new Scene(root, 900, 900 );
+        stage.setScene(scene);
 
 
-        Scene scene = new Scene(root, 845, 845);
-        //scene.setFill(backgroundColor);
 
-        Image icon = new Image(getClass().getResourceAsStream(LOGO));
+        @SuppressWarnings("ConstantConditions")
+        Image icon = new Image(getClass().getResourceAsStream("/be/ugent/objprog/ugentopoly/assets/" + ICON));
         stage.getIcons().add(icon);
         stage.setFullScreen(false);
-
+        stage.setMinWidth(845); // Set minimum width of the window
+        stage.setMinHeight(845); // Set minimum height of the window
+        stage.show();
 
         stage.setTitle("Ugentopoly");
         stage.setOnCloseRequest(e -> dice.close());
