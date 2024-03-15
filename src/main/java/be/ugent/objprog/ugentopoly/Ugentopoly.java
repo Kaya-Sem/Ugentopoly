b593ee445e7446d9326b0f02f7d317f037df9c13
Kaya-Sem <kayasemvc@gmail.com>
2024-03-15 11:22:27 +0100

    updateupda

--- src/main/java/be/ugent/objprog/ugentopoly/Ugentopoly.java
+++ src/main/java/be/ugent/objprog/ugentopoly/Ugentopoly.java
@@ -3,25 +3,37 @@ package be.ugent.objprog.ugentopoly;
 import be.ugent.objprog.dice.Dice;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Ugentopoly extends Application {
+    static final String LOGO = "/be/ugent/objprog/ugentopoly/assets/tax.png";
     @Override
     public void start(Stage stage) {
-        final Dice dice = new Dice();
 
-        Button btn = new Button();
-        btn.setText("Rol dobbelstenen");
-        btn.setOnAction(event -> dice.roll(t -> System.out.println("Klaar met rollen")));
+
+
+        final Dice dice = new Dice();
+        //Color backgroundColor = Color.rgb(0,  255, 202);
 
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
 
-        Scene scene = new Scene(root, 300, 250);
 
-        stage.setTitle("Dobbelstenen");
+
+
+
+
+        Scene scene = new Scene(root, 845, 845);
+        //scene.setFill(backgroundColor);
+
+        Image icon = new Image(getClass().getResourceAsStream(LOGO));
+        stage.getIcons().add(icon);
+        stage.setFullScreen(false);
+
+
+        stage.setTitle("Ugentopoly");
         stage.setOnCloseRequest(e -> dice.close());
         stage.setScene(scene);
         stage.show();
