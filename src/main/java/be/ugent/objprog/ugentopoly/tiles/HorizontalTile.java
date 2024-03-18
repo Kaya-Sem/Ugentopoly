f5029f126f4630bea82bf2e5f9ea5c73c6f85404
Kaya-Sem <kayasemvc@gmail.com>
2024-03-18 16:22:29 +0100

    corrected order

--- src/main/java/be/ugent/objprog/ugentopoly/tiles/HorizontalTile.java
+++ src/main/java/be/ugent/objprog/ugentopoly/tiles/HorizontalTile.java
@@ -2,8 +2,8 @@ package be.ugent.objprog.ugentopoly.tiles;
 
 public class HorizontalTile extends Tile{
     public HorizontalTile(){
-        setPrefHeight(845.0/13);
-        setPrefWidth(845.0/13 *2);
+        setPrefWidth((845.0 / 13) *2);
+        setPrefHeight(845.0 / 13);
         setText("Button");
     }
 }
