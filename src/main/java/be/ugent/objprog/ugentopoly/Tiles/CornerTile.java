fbd6839ca6012e2716dd1fb9dd7bcd7cb89c70df
Kaya-Sem <kayasemvc@gmail.com>
2024-03-19 18:21:40 +0100

    new label

--- src/main/java/be/ugent/objprog/ugentopoly/Tiles/CornerTile.java
+++ src/main/java/be/ugent/objprog/ugentopoly/Tiles/CornerTile.java
@@ -1,11 +1,17 @@
 package be.ugent.objprog.ugentopoly.Tiles;
 
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+
 public class CornerTile extends Tile{
     public CornerTile() {
         super.
         setPrefHeight(Tile.LONG_SIDE);
         setPrefWidth(Tile.LONG_SIDE);
+        setAlignment(Pos.CENTER);
 
-        // setText("corner tile");
+        Label text = new Label("cornertile");
+        getChildren().addAll(text);
+
     }
 }
