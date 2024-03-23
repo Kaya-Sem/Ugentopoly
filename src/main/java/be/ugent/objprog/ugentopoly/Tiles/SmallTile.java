12f3f6ad316361ac8feb0d106ebe2e080d80431e
Kaya-Sem <kayasemvc@gmail.com>
2024-03-23 19:58:44 +0100

    correction rotation

--- src/main/java/be/ugent/objprog/ugentopoly/Tiles/SmallTile.java
+++ src/main/java/be/ugent/objprog/ugentopoly/Tiles/SmallTile.java
@@ -4,12 +4,21 @@ public class SmallTile extends Tile {
 
     protected int rotatie;
 
-    public SmallTile(int rotatie){
+    public SmallTile(int rotatie) {
         this.rotatie = rotatie;
-        setRotate(rotatie);
         setMinHeight(Tile.SHORT_SIDE);
         setMaxHeight(Tile.SHORT_SIDE);
         setMinWidth(Tile.LONG_SIDE);
         setMaxWidth(Tile.LONG_SIDE);
-}
+
+        // set pivot points
+        setTranslateX(getWidth() / 2);
+        setTranslateY(getHeight() / 2);
+        setRotate(rotatie);
+        if (rotatie == 90 || rotatie == 270) {
+
+            setTranslateX(-32.5);
+            setTranslateY(32.5);
+        }
+    }
 }
