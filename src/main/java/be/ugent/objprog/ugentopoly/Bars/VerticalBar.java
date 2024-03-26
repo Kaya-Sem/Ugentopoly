c51489a09af87e69b6f4ce0e4a10376bc7ffa073
Kaya-Sem <kayasemvc@gmail.com>
2024-03-26 21:27:35 +0100

    formatting

--- src/main/java/be/ugent/objprog/ugentopoly/Bars/VerticalBar.java
+++ src/main/java/be/ugent/objprog/ugentopoly/Bars/VerticalBar.java
@@ -7,11 +7,16 @@ import javafx.scene.layout.RowConstraints;
 
 import java.util.List;
 
+/*
+    initializeRowConstraints() : fill the bar with 8 rows, as to create 8 places for tiles.
+    populate() : places 9 received tiles in the slots.
+*/
+
 public class VerticalBar extends GridPane implements Bar {
     private static final int NUM_ROWS = 8;
     private static final double ROW_HEIGHT = Tile.SHORT_SIDE;
 
-    public VerticalBar(){
+    public VerticalBar() {
         setPrefWidth(Tile.LONG_SIDE);
         setPrefHeight(MiddleSection.getSize());
         initializeRowConstraints();
@@ -23,9 +28,7 @@ public class VerticalBar extends GridPane implements Bar {
         }
     }
 
-
-    // Fill the bar with the nine received tiles.
-    public void populate(List<? extends Tile> tiles){
+    public void populate(List<? extends Tile> tiles) {
         for (int i = 0; i < 9; i++) {
             Tile tile = tiles.get(i);
             add(tile, 0, i);
