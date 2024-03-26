9b6ad1118e133e53508aec23a39bc630e395f7e0
Kaya-Sem <kayasemvc@gmail.com>
2024-03-26 21:25:08 +0100

    shortened and cleaned up

--- src/main/java/be/ugent/objprog/ugentopoly/Bars/VerticalBar.java
+++ src/main/java/be/ugent/objprog/ugentopoly/Bars/VerticalBar.java
@@ -8,29 +8,27 @@ import javafx.scene.layout.RowConstraints;
 import java.util.List;
 
 public class VerticalBar extends GridPane implements Bar {
-    private static final RowConstraints[] ROW_CONSTRAINTS = {
-            new RowConstraints(Tile.SHORT_SIDE),
-            new RowConstraints(Tile.SHORT_SIDE),
-            new RowConstraints(Tile.SHORT_SIDE),
-            new RowConstraints(Tile.SHORT_SIDE),
-            new RowConstraints(Tile.SHORT_SIDE),
-            new RowConstraints(Tile.SHORT_SIDE),
-            new RowConstraints(Tile.SHORT_SIDE),
-            new RowConstraints(Tile.SHORT_SIDE),
-            // TODO maybe create a class or enum for this, too much repetition
-    };
-
+    private static final int NUM_ROWS = 8;
+    private static final double ROW_HEIGHT = Tile.SHORT_SIDE;
 
     public VerticalBar(){
         setPrefWidth(Tile.LONG_SIDE);
         setPrefHeight(MiddleSection.getSize());
-        getRowConstraints().addAll(ROW_CONSTRAINTS);
+        initializeRowConstraints();
     }
+
+    private void initializeRowConstraints() {
+        for (int i = 0; i < NUM_ROWS; i++) {
+            getRowConstraints().add(new RowConstraints(ROW_HEIGHT));
+        }
+    }
+
+
+    // Fill the bar with the nine received tiles.
     public void populate(List<? extends Tile> tiles){
         for (int i = 0; i < 9; i++) {
             Tile tile = tiles.get(i);
             add(tile, 0, i);
         }
-
     }
 }
