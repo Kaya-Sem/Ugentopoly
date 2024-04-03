fd9e6d98a26f1ef4afebcded9d64c90d23468069
Kaya-Sem <kayasemvc@gmail.com>
2024-04-03 22:45:21 +0200

    removed unneeded imports

--- src/main/java/be/ugent/objprog/ugentopoly/TileButton.java
+++ src/main/java/be/ugent/objprog/ugentopoly/TileButton.java
@@ -1,11 +1,6 @@
 package be.ugent.objprog.ugentopoly;
 
 import be.ugent.objprog.ugentopoly.GameBoard.Board;
-import be.ugent.objprog.ugentopoly.GameBoard.MiddleSection;
-import be.ugent.objprog.ugentopoly.Tiles.Tile.Tile;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.control.ToggleButton;
 
 public class TileButton extends ToggleButton {
@@ -16,14 +11,4 @@ public class TileButton extends ToggleButton {
         setToggleGroup(Board.TOGGLE_GROUP);
         setStyle("-fx-background-color: transparent;");
     }
-
-    /* MVC model? set a current present card current game state: selected card: None/VerticalCard
-    static class ButtonHandler implements EventHandler<ActionEvent> {
-        @Override
-        public void handle(ActionEvent event) {
-
-
-            System.err.println((isSelected) ? this + " Button selected" : this + "Button deselected");
-        }
-    }*/
 }
