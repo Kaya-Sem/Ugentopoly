2752794e1667a35911a9c1b53cc5fc30bf9977ed
Kaya-Sem <kayasemvc@gmail.com>
2024-04-03 16:50:20 +0200

    updates and removal unneeded

--- src/main/java/be/ugent/objprog/ugentopoly/Tiles/Tile/CornerTiles/CornerTile.java
+++ src/main/java/be/ugent/objprog/ugentopoly/Tiles/Tile/CornerTiles/CornerTile.java
@@ -1,18 +1,8 @@
 package be.ugent.objprog.ugentopoly.Tiles.Tile.CornerTiles;
 
-import be.ugent.objprog.ugentopoly.TileButton;
 import be.ugent.objprog.ugentopoly.Tiles.Tile.Tile;
 
-import java.util.Map;
-
 abstract public class CornerTile extends Tile {
-   final TileButton tileButton = new TileButton();
-
-    protected static final Map<String, String> IMAGES = Map.of(
-            "tile.jail", "/be/ugent/objprog/ugentopoly/assets/jail.png",
-            "tile.freeparking", "/be/ugent/objprog/ugentopoly/assets/free_parking.png",
-            "tile.gotojail", "/be/ugent/objprog/ugentopoly/assets/go_to_jail.png"
-    );
 
     public CornerTile(Record companion, String id) {
         super(companion, id);
@@ -20,7 +10,5 @@ abstract public class CornerTile extends Tile {
         setMaxHeight(Tile.LONG_SIDE);
         setMaxWidth(Tile.LONG_SIDE);
         setMinWidth(Tile.LONG_SIDE);
-
-        tileButton.setOnAction(event -> handleButton(event, companion));
     }
 }
