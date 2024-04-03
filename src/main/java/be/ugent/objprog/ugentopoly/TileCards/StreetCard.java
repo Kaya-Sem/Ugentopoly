c0dde419441d014da7cc001cdb27750470918d9c
Kaya-Sem <kayasemvc@gmail.com>
2024-04-03 22:18:29 +0200

    working street

--- src/main/java/be/ugent/objprog/ugentopoly/TileCards/StreetCard.java
+++ src/main/java/be/ugent/objprog/ugentopoly/TileCards/StreetCard.java
@@ -6,19 +6,22 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
+
+import static javafx.geometry.Pos.*;
+import static javafx.geometry.Pos.CENTER_RIGHT;
 
 public class StreetCard extends HorizontalCard {
     public StreetCard(String text, String color, StreetCompanion c) {
 
-        HBox hBox = new HBox();
-        hBox.setAlignment(Pos.CENTER);
-        hBox.setPadding(new Insets(20, 20, 20, 20));
 
 
         Label textLabel = new Label(text);
@@ -27,25 +30,21 @@ public class StreetCard extends HorizontalCard {
         Label cost = new Label(c.cost() + "€");
         cost.setFont(Font.font("Arial", FontWeight.THIN, 11));
 
-        HBox rent1 = new rentLabel("level 1", c.rent1());
-        HBox rent2 = new rentLabel("level 2", c.rent2());
-        HBox rent3 = new rentLabel("level 3", c.rent3());
-        HBox rent4 = new rentLabel("level 4", c.rent4());
-        HBox rent5 = new rentLabel("level 5", c.rent5());
-
         VBox info = new VBox(
                 textLabel,
                 cost,
                 new Label("\n"),
-                new rentLabel("level 0", c.rent0()),
-                rent1,
-                rent2,
-                rent3,
-                rent4,
-                rent5
+                new rentLabel("rent 0", c.rent0()),
+                new rentLabel("rent 1", c.rent1()),
+                new rentLabel("rent 2", c.rent2()),
+                new rentLabel("rent 3", c.rent3()),
+                new rentLabel("rent 4", c.rent4()),
+                new rentLabel("rent 5", c.rent5())
         );
 
-        info.setAlignment(Pos.CENTER_LEFT);
+        info.setAlignment(CENTER_LEFT);
+        info.setMaxWidth(WIDTH / 2);
+//        info.setPadding(new Insets(20, 20, 20, 20));
 
         Rectangle bar = new Rectangle(Tile.SHORT_SIDE - 3, HEIGHT - 3);
         bar.setFill(Paint.valueOf(color));
@@ -53,10 +52,12 @@ public class StreetCard extends HorizontalCard {
         bar.setArcWidth(20);
 
 
-        hBox.getChildren().addAll(info);
-        hBox.setAlignment(Pos.CENTER_LEFT);
+        HBox hBox = new HBox(info);
+        hBox.setAlignment(CENTER);
+        hBox.setPadding(new Insets(20, 20, 20, 20));
+        hBox.setAlignment(CENTER_LEFT);
 
-        setAlignment(bar, Pos.CENTER_RIGHT);
+        setAlignment(bar, CENTER_RIGHT);
 
         getChildren().addAll(hBox, bar);
     }
@@ -66,10 +67,14 @@ public class StreetCard extends HorizontalCard {
         public rentLabel(String preText, String rentAmount){
             Label text = new Label(preText);
             Label amount = new Label(rentAmount + "€");
-            amount.setAlignment(Pos.CENTER_RIGHT);
-            setAlignment(Pos.CENTER_LEFT);
+            amount.setFont(Font.font("Arial", FontWeight.BOLD, 13));
+            Region region = new Region();
+
+            setHgrow(region, Priority.ALWAYS);
+
+            setAlignment(CENTER_LEFT);
             setSpacing(30);
-        getChildren().addAll(text, amount);
+        getChildren().addAll(text, region, amount);
         }
     }
 }
