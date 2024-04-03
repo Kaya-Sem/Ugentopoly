9396db6f6154867f7b5af2f20ed52c7462fe3c88
Kaya-Sem <kayasemvc@gmail.com>
2024-04-03 20:15:25 +0200

    updated custom handler

--- src/main/java/be/ugent/objprog/ugentopoly/CustomButtonHandler.java
+++ src/main/java/be/ugent/objprog/ugentopoly/CustomButtonHandler.java
@@ -1,13 +1,12 @@
 package be.ugent.objprog.ugentopoly;
 
+import be.ugent.objprog.ugentopoly.TileCards.TemplateCard;
 import be.ugent.objprog.ugentopoly.GameBoard.MiddleSection;
-import javafx.scene.layout.StackPane;
 
 public class CustomButtonHandler {
     public static MiddleSection middleSection = null;
 
-    public static void updateDisplayedCard(Record companion) {
-        StackPane pane = new StackPane();
-        middleSection.updateDisplayedCard(pane);
+    public static void updateDisplayedCard(TemplateCard card) {
+        middleSection.updateDisplayedCard(card);
     }
 }
