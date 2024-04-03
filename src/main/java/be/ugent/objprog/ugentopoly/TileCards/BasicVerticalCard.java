a442605d4d2004ffb32b0db156c032f7561144c1
Kaya-Sem <kayasemvc@gmail.com>
2024-04-03 22:37:18 +0200

    init

---
+++ src/main/java/be/ugent/objprog/ugentopoly/TileCards/BasicVerticalCard.java
@@ -0,0 +1,32 @@
+package be.ugent.objprog.ugentopoly.TileCards;
+
+import be.ugent.objprog.ugentopoly.Tiles.Tile.Tile;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public class BasicVerticalCard extends VerticalCard {
+
+    public BasicVerticalCard(Image image, String text) {
+
+        Label textLabel = new Label(text);
+        textLabel.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+
+        ImageView imageView = new ImageView(image);
+        imageView.setPreserveRatio(true);
+        imageView.setFitHeight(Tile.LONG_SIDE);
+        imageView.setFitWidth(Tile.LONG_SIDE);
+
+        VBox vBox = new VBox(imageView, textLabel);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setPrefHeight(Double.MAX_VALUE);
+        vBox.setPrefWidth(Double.MAX_VALUE);
+        vBox.setSpacing(10);
+
+        getChildren().addAll(vBox);
+    }
+}
---
+++ src/main/java/be/ugent/objprog/ugentopoly/TileCards/TemplateCard.java
@@ -0,0 +1,6 @@
+package be.ugent.objprog.ugentopoly.TileCards;
+
+import javafx.scene.layout.StackPane;
+
+public class TemplateCard extends StackPane {
+}
