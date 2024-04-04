706316a3d3ccc40d34f14be497a7b15802489889
Kaya-Sem <kayasemvc@gmail.com>
2024-04-04 12:22:39 +0200

    updates

--- src/main/java/be/ugent/objprog/ugentopoly/tiles/tile/Tile.java
+++ src/main/java/be/ugent/objprog/ugentopoly/tiles/tile/Tile.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.StackPane;
 public class Tile extends StackPane {
     public final static double LONG_SIDE = ((Ugentopoly.BOARD_SIZE / Ugentopoly.SMALL_TILES) * 2);
     public final static double SHORT_SIDE = ((Ugentopoly.BOARD_SIZE / Ugentopoly.SMALL_TILES));
+    private static final double OFFSET = 32.5;
 
     final Record companion;
     final String id;
@@ -26,14 +27,12 @@ public class Tile extends StackPane {
     protected void setup(String id) {}
 
     public void applyRotation(double angle){
-        // TODO fix rotation
-        // set pivot points as constant
         setTranslateX(getWidth() / 2);
         setTranslateY(getHeight() / 2);
         setRotate(angle);
         if (angle == 90 || angle == 270) {
-            setTranslateX(-32.5);
-            setTranslateY(32.5);
+            setTranslateX(- OFFSET);
+            setTranslateY(OFFSET);
         }
     }
 
