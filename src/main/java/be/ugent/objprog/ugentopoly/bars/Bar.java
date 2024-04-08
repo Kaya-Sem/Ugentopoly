4d6bd693c168bf273faaadf543e9868b7e871a76
Kaya-Sem <kayasemvc@gmail.com>
2024-04-08 22:09:52 +0200

    removed parameter

--- src/main/java/be/ugent/objprog/ugentopoly/bars/Bar.java
+++ src/main/java/be/ugent/objprog/ugentopoly/bars/Bar.java
@@ -5,7 +5,7 @@ import be.ugent.objprog.ugentopoly.tiles.tile.Tile;
 import java.util.List;
 
 public interface Bar {
-    void populate(List<? extends Tile> tiles);
+    void populate();
 
     void applyRotation(double angle);
 }
