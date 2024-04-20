2a7be48c0d74b931cd0c09e1f8126e3afb800ebb
Kaya-Sem <kayasemvc@gmail.com>
2024-04-20 20:46:22 +0200

    made not abstract and cleaned up

--- src/main/java/be/ugent/objprog/ugentopoly/tiles/tileViews/cornerTiles/CornerTile.java
+++ src/main/java/be/ugent/objprog/ugentopoly/tiles/tileViews/cornerTiles/CornerTile.java
@@ -4,13 +4,13 @@ import be.ugent.objprog.ugentopoly.tiles.tileViews.Tile;
 import be.ugent.objprog.ugentopoly.tiles.tileModels.TileModel;
 import javafx.beans.InvalidationListener;
 
-abstract public class CornerTile extends Tile implements InvalidationListener {
+public class CornerTile extends Tile {
 
     public CornerTile(TileModel model) {
         super(model);
-        setMinHeight(Tile.LONG_SIDE);
-        setMaxHeight(Tile.LONG_SIDE);
-        setMaxWidth(Tile.LONG_SIDE);
-        setMinWidth(Tile.LONG_SIDE);
+        setMinHeight(LONG_SIDE);
+        setMaxHeight(LONG_SIDE);
+        setMaxWidth(LONG_SIDE);
+        setMinWidth(LONG_SIDE);
     }
 }
