68c7076c1613be8dd630732ef762c5c98c247cb0
Kaya-Sem <kayasemvc@gmail.com>
2024-04-21 19:49:09 +0200

    created model for jail

---
+++ src/main/java/be/ugent/objprog/ugentopoly/tiles/tileModels/JailTileModel.java
@@ -0,0 +1,7 @@
+package be.ugent.objprog.ugentopoly.tiles.tileModels;
+
+public class JailTileModel extends TileModel{
+    public JailTileModel(String id, int position) {
+        super(id, position);
+    }
+}
