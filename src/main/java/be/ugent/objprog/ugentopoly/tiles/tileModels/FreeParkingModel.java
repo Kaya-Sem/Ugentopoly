67b9ca26ad62f9ab742880a75eb860d8bde8f800
Kaya-Sem <kayasemvc@gmail.com>
2024-04-21 19:41:56 +0200

    created model for free parking tile

---
+++ src/main/java/be/ugent/objprog/ugentopoly/tiles/tileModels/FreeParkingModel.java
@@ -0,0 +1,7 @@
+package be.ugent.objprog.ugentopoly.tiles.tileModels;
+
+public class FreeParkingModel extends TileModel{
+    public FreeParkingModel(String id, int position) {
+        super(id, position);
+    }
+}
