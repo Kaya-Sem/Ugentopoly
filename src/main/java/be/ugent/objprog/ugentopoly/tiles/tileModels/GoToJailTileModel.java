d9e5e779cc02e9428c3d0fa42a7692796b271578
Kaya-Sem <kayasemvc@gmail.com>
2024-04-21 19:46:10 +0200

    created model for go to jail

---
+++ src/main/java/be/ugent/objprog/ugentopoly/tiles/tileModels/GoToJailTileModel.java
@@ -0,0 +1,7 @@
+package be.ugent.objprog.ugentopoly.tiles.tileModels;
+
+public class GoToJailTileModel extends TileModel{
+    public GoToJailTileModel(String id, int position) {
+        super(id, position);
+    }
+}
