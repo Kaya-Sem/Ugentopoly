9ed7c315a52f20c790efc38ed2f7893be4d5a168
Kaya-Sem <kayasemvc@gmail.com>
2024-04-23 21:00:15 +0200

    init ChanceCardFactory

---
+++ src/main/java/be/ugent/objprog/ugentopoly/factories/ChanceCardFactory.java
@@ -0,0 +1,93 @@
+package be.ugent.objprog.ugentopoly.factories;
+
+import be.ugent.objprog.ugentopoly.GameModel;
+import be.ugent.objprog.ugentopoly.cardDeck.GameCard;
+import be.ugent.objprog.ugentopoly.players.Pion;
+import be.ugent.objprog.ugentopoly.players.PlayerModel;
+
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class ChanceCardFactory<T> {
+    protected static final int TILES = 40;
+    private Map<String, String> data = null;
+
+    private final Map<String, Supplier<BiConsumer<GameModel, GameCard>>> actions = Map.of(
+            "JAIL", this::jail,
+            "MOVE", this::move,
+            "MOVEREL", this::moveRel,
+            "MONEY", this::money,
+            "PLAYERS_MONEY", this::playersMoney
+    );
+
+    public GameCard forge(Map<String, String> tileData) {
+        // add id to chancecard
+        data = tileData;
+        Supplier action = actions.get(tileData.get("id"));
+        GameCard card = new GameCard();
+        card.setCardAction((BiConsumer<GameModel, GameCard>) action.get());
+        return card;
+    }
+
+    private BiConsumer<GameModel, GameCard> jail() {
+        return (gameModel, chanceCard) -> {
+                PlayerModel player =gameModel.getCurrentPlayerMove();
+                player.setGetOutOfJailCard(chanceCard);
+                gameModel.getChanceCardDeck().removeCard(chanceCard);
+                // TODO show card via gamemodel
+            };
+    }
+
+    private BiConsumer<GameModel, GameCard> move() {
+        return ((gameModel, chanceCard) -> {
+            boolean collect = Boolean.parseBoolean(data.get("collect"));
+            int newPosition = Integer.parseInt(data.get("position"));
+
+            Pion pion = gameModel.getCurrentPlayerMove().getPion();
+            gameModel.getTileModels()[pion.getPosition()].removePion(pion);
+
+            // if it would pass start, call start tile's action!
+            if (TILES < newPosition + pion.getPosition() && collect) {
+                Consumer<GameModel> action = gameModel.getTileModels()[0].getPlayerTileInteraction();
+                action.accept(gameModel);
+            }
+
+            pion.setPosition(newPosition);
+            gameModel.getTileModels()[newPosition].addPion(pion);
+            // NEEDSLOG
+        });
+    }
+
+    private BiConsumer<GameModel, GameCard> moveRel() {
+        return ((gameModel, chanceCard) -> {
+            int relPosition = Integer.parseInt(data.get("relative"));
+
+            Pion pion = gameModel.getCurrentPlayerMove().getPion();
+            gameModel.getTileModels()[pion.getPosition()].removePion(pion);
+
+            pion.setPosition(pion.getPosition() + relPosition);
+            gameModel.getTileModels()[pion.getPosition()].addPion(pion);
+            // NEEDSLOG
+        });
+    }
+
+    private BiConsumer<GameModel, GameCard> money() {
+        return (((gameModel, chanceCard) -> {
+            int amount = Integer.parseInt(data.get("amount"));
+            gameModel.getCurrentPlayerMove().changeBalance(amount);
+        }));
+        // NEEDSLOG
+    }
+
+    private BiConsumer<GameModel, GameCard> playersMoney() {
+        return (((gameModel, chanceCard) -> {
+            int amount = Integer.parseInt(data.get("amount"));
+            gameModel.getPlayerModels().forEach(player -> player.changeBalance(-amount));
+        }));
+    }
+
+    // create methods that takes gamemodel and shows it on the board
+
+}
