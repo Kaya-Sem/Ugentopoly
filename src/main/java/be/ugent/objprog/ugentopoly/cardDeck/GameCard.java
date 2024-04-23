afbb6282375213786973283debd4c95d1afcae2c
Kaya-Sem <kayasemvc@gmail.com>
2024-04-23 20:51:25 +0200

    renamed and removed execute function,

---
+++ src/main/java/be/ugent/objprog/ugentopoly/cardDeck/GameCard.java
@@ -0,0 +1,26 @@
+package be.ugent.objprog.ugentopoly.cardDeck;
+
+import be.ugent.objprog.ugentopoly.GameModel;
+import be.ugent.objprog.ugentopoly.tiles.tileCards.VerticalCard;
+
+import java.util.function.BiConsumer;
+
+public class GameCard extends VerticalCard {
+    private BiConsumer<GameModel, GameCard> cardAction;
+    // Kanskaart heeft een BiConsumer? neemt een playermodel en een card.
+    // de biconsumer bepaalt een bepaalde actie die moet gebeuren, met
+
+    public GameCard() {
+        cardAction = (gameModel, chanceCard) -> {
+            throw new RuntimeException("cardmethod asked but had none");
+        };
+    }
+
+    public void setCardAction(BiConsumer<GameModel, GameCard> consumer) {
+        cardAction = consumer;
+    }
+
+    public BiConsumer getCardAction() {
+        return cardAction;
+    }
+}
