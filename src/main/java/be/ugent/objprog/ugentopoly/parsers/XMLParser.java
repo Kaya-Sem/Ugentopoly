4f29f513e402cec96dd2a7ac9bb4816288d06d12
Kaya-Sem <kayasemvc@gmail.com>
2024-04-24 17:05:06 +0200

    added parsing for cards + refactor

--- src/main/java/be/ugent/objprog/ugentopoly/parsers/XMLParser.java
+++ src/main/java/be/ugent/objprog/ugentopoly/parsers/XMLParser.java
@@ -2,42 +2,78 @@ package be.ugent.objprog.ugentopoly.parsers;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 
-public class XMLParser {
-    /*
-     * NON-URGENT: write out XMLParser documentation
-     */
+/*
+ * NON-URGENT: write out XMLParser documentation
+ */
 
+public class XMLParser {
     // TODO parse make parsers for other xml data
 
     private static final String XML_PATH = "/be/ugent/objprog/ugentopoly/ugentopoly.xml";
-    static final String[] ATTRIBUTES = { "area", "cost", "rent", "rent0", "rent1", "rent2", "rent3", "rent4", "rent5",
-            "amount" };
+    static final String[] ATTRIBUTES = { "area", "cost", "rent", "rent0", "rent1",
+                                        "rent2", "rent3", "rent4", "rent5",
+                                        "amount" };
     Document document;
     Element root;
 
-    public XMLParser() {
+    public XMLParser() throws JDOMException {
         InputStream inputStream = getClass().getResourceAsStream(XML_PATH);
         try {
             document = new SAXBuilder().build(inputStream);
             root = document.getRootElement();
-        } catch (JDOMException | IOException e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public String getStartingBalance() {
         Element settings = root.getChild("settings");
         return settings.getAttributeValue("balance");
+    }
+
+    public Map<String, Map<String, String>> getChanceCards() {
+        return getDeckCards("CHANCE");
+    }
+
+    public Map<String, Map<String, String>> getChestCards() {
+        return getDeckCards("CHEST");
+    }
+
+    private Map<String, Map<String, String>> getDeckCards(String deckType) {
+        Map<String, Map<String, String>> cards = new HashMap<>();
+        List<Element> decks = root.getChildren("deck");
+        List<Element> cardElements = new ArrayList<>();
+
+        decks.stream().filter(deck -> deck.getAttributeValue("type").equals(deckType)).forEach(deck -> {
+            cardElements.addAll(deck.getChildren("card"));
+            cardElements.forEach(card -> {
+                Map<String, String> cardAttributes = new HashMap<>();
+
+                String cardId = card.getAttributeValue("id");
+                cardAttributes.put("id", cardId);
+                cardAttributes.put("type", card.getAttributeValue("type"));
+
+                card.getAttributes().forEach(attribute -> cardAttributes.put(attribute.getName(), attribute.getValue()));
+
+                // Filter out null values before putting the card in the map
+                cards.put(cardId, cardAttributes.entrySet().stream()
+                        .filter(e -> null != e.getValue())
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+
+            });
+        });
+        ;
+
+        return cards;
     }
 
     public int getStartAmount() {
@@ -55,12 +91,12 @@ public class XMLParser {
             throw new IllegalStateException("tile data has not correctly been parsed from XML file. Nothing was returned");
         }
 
-        for (Element tileElement : tileElements) {
-            // Parse tileViews attributes and create tileViews data map
+        // Parse tileViews attributes and create tileViews data map
+        tileElements.forEach(tileElement -> {
             String tileIdentifier = tileElement.getAttributeValue("position");
             Map<String, String> tileAttributes = parseTile(tileElement);
             tiles.put(tileIdentifier, tileAttributes);
-        }
+        });
 
         return tiles;
     }
@@ -91,21 +127,4 @@ public class XMLParser {
         return colors;
     }
 
-    // REMOVE
-    private static void test() {
-        XMLParser parser = new XMLParser();
-        Map<String, Map<String, String>> tilesData = parser.parseAllTileData();
-
-        System.out.println(tilesData.size());
-
-        tilesData.entrySet().forEach(entry -> System.out.printf("%-15s : %s%n", entry.getKey(), entry.getValue()));
-
-        System.out.println("Starting balance: " + parser.getStartingBalance());
-        System.out.println("Start tile amount: " + parser.getStartAmount());
-        System.out.println(parser.areaColors());
-    }
-
-    public static void main(String[] args) {
-        test();
-    }
 }
