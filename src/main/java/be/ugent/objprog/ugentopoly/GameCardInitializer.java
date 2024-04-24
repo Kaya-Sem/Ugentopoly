89e4ae3f8ad87e2e1eeef0e59c4504e7b5fa81e3
Kaya-Sem <kayasemvc@gmail.com>
2024-04-24 16:36:05 +0200

    created class to generate two cardsdecks

---
+++ src/main/java/be/ugent/objprog/ugentopoly/GameCardInitializer.java
@@ -0,0 +1,39 @@
+package be.ugent.objprog.ugentopoly;
+
+import be.ugent.objprog.ugentopoly.cardDeck.CardDeck;
+import be.ugent.objprog.ugentopoly.cardDeck.GameCard;
+import be.ugent.objprog.ugentopoly.factories.GameCardFactory;
+import be.ugent.objprog.ugentopoly.parsers.XMLParser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class GameCardInitializer {
+
+    private final CardDeck chanceCards;
+    private final CardDeck chestCards;
+
+    public GameCardInitializer(XMLParser parser, GameCardFactory factory) {
+        Map<String, Map<String, String>> chanceCardsMap = parser.getChanceCards();
+        Map<String, Map<String, String>> chestCardsMap = parser.getChestCards();
+
+        chanceCards = forgeGameCards(chanceCardsMap, factory);
+        chestCards = forgeGameCards(chestCardsMap, factory);
+    }
+
+    private static CardDeck forgeGameCards(Map<String, Map<String, String>> cardDataMap, GameCardFactory factory) {
+        List<GameCard> cards = new ArrayList<>();
+        cardDataMap.values().forEach(cardData -> cards.add(factory.forge(cardData)));
+
+        return new CardDeck(cards);
+    }
+
+    public CardDeck getChanceCards() {
+        return chanceCards;
+    }
+
+    public CardDeck getChestCards() {
+        return chestCards;
+}
+}
