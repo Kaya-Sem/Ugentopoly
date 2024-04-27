2e73f80a208acb6243510d3f71e25cbfe1ae379d
Kaya-Sem <kayasemvc@gmail.com>
2024-04-27 11:52:58 +0200

    implemented action: paying tax to bonuspot

--- src/main/java/be/ugent/objprog/ugentopoly/tiles/tileModels/TaxTileModel.java
+++ src/main/java/be/ugent/objprog/ugentopoly/tiles/tileModels/TaxTileModel.java
@@ -1,9 +1,27 @@
 package be.ugent.objprog.ugentopoly.tiles.tileModels;
 
+import be.ugent.objprog.ugentopoly.GameModel;
+
+import java.util.function.Consumer;
+
 public class TaxTileModel extends TileModel{
     private final int amount;
+
     public TaxTileModel(String id, int position, int amount) {
         super(id, position);
         this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public Consumer<GameModel> getPlayerTileInteraction() {
+        return (gameModel -> {
+            gameModel.changeBonusPot(this.amount);
+            gameModel.getCurrentPlayerMove().changeBalance(-amount);
+            System.out.println("taxtilemodel");
+        });
     }
 }
