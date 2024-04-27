6f5a7f290d045e6775ca145f924f827cbe3792a8
Kaya-Sem <kayasemvc@gmail.com>
2024-04-27 15:17:12 +0200

    added player tile interaction lambda

--- src/main/java/be/ugent/objprog/ugentopoly/tiles/tileModels/FreeParkingModel.java
+++ src/main/java/be/ugent/objprog/ugentopoly/tiles/tileModels/FreeParkingModel.java
@@ -1,7 +1,24 @@
 package be.ugent.objprog.ugentopoly.tiles.tileModels;
 
+import be.ugent.objprog.ugentopoly.GameModel;
+import be.ugent.objprog.ugentopoly.players.PlayerModel;
+
+import java.util.function.Consumer;
+
 public class FreeParkingModel extends TileModel{
     public FreeParkingModel(String id, int position) {
         super(id, position);
+    }
+
+    @Override
+    public Consumer<GameModel> getPlayerTileInteraction() {
+        return gameModel -> {
+            PlayerModel currentPlayer = gameModel.getCurrentPlayerMove();
+            int amount = gameModel.getBonusPot();
+            gameModel.setBonusPot(0);
+            currentPlayer.changeBalance(amount);
+
+            gameModel.addLog(currentPlayer.getPlayerName(), "kreeg " + amount + "€ uit de bonuspot!");
+        };
     }
 }
