3d16ed607e055dd8a7e699fcfbc69b4c5c50451c
Kaya-Sem <kayasemvc@gmail.com>
2024-04-27 15:02:09 +0200

    made name bold

--- src/main/java/be/ugent/objprog/ugentopoly/logging/LogElement.java
+++ src/main/java/be/ugent/objprog/ugentopoly/logging/LogElement.java
@@ -2,14 +2,18 @@ package be.ugent.objprog.ugentopoly.logging;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 
 public class LogElement extends HBox {
 
     public LogElement(String playerName, String actionPerformed) {
         Label name = new Label(playerName);
+        name.setFont(Font.font("Arial", FontWeight.BOLD, 11));
         Label action = new Label(actionPerformed);
 
+        // TODO add styling
         getChildren().addAll(name, action);
     }
 }
