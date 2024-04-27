176757e9efeccc36372c0101352b160d29b0cbe7
Kaya-Sem <kayasemvc@gmail.com>
2024-04-27 12:59:56 +0200

    added logging when a player passes

--- src/main/java/be/ugent/objprog/ugentopoly/tiles/tileModels/JailTileModel.java
+++ src/main/java/be/ugent/objprog/ugentopoly/tiles/tileModels/JailTileModel.java
@@ -1,7 +1,20 @@
 package be.ugent.objprog.ugentopoly.tiles.tileModels;
 
+import be.ugent.objprog.ugentopoly.GameModel;
+
+import java.util.function.Consumer;
+
 public class JailTileModel extends TileModel{
+
     public JailTileModel(String id, int position) {
         super(id, position);
     }
+
+    @Override
+    public Consumer<GameModel> getPlayerTileInteraction() {
+        return gameModel -> {
+            gameModel.addLog(gameModel.getCurrentPlayerMove().getPlayerName(), "landed on Overpoort, but couldn't stay");
+        };
+    }
+
 }
