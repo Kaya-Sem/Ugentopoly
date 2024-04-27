abca2814d2cf2be50c886ca8be59bb510f1e033f
Kaya-Sem <kayasemvc@gmail.com>
2024-04-27 11:34:41 +0200

    extraced the tab player info view

---
+++ src/main/java/be/ugent/objprog/ugentopoly/playertabpane/PlayerTabInfoView.java
@@ -0,0 +1,34 @@
+package be.ugent.objprog.ugentopoly.playertabpane;
+
+import be.ugent.objprog.ugentopoly.players.PlayerModel;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class PlayerTabInfoView extends VBox implements InvalidationListener {
+
+    private final PlayerModel playerModel;
+    private final StringProperty playerBalance = new SimpleStringProperty();
+
+    public PlayerTabInfoView(PlayerModel playerModel) {
+        this.playerModel = playerModel;
+        playerModel.addListener(this);
+        Label balance = new Label();
+        balance.textProperty().bind(playerBalance);
+
+        // TODO add player position and style balance better
+        // TODO add listview of tiles
+
+        // ObservableList<TileModel> observableList = FXCollections.observableArrayList(playerModel.getOwnedTiles());
+        getChildren().addAll(balance);
+    }
+
+    @Override
+    public void invalidated(Observable observable) {
+        // TODO
+        playerBalance.set(String.valueOf(playerModel.getBalance()));
+    }
+}
