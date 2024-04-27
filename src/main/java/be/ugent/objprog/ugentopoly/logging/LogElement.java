3c45d4a709f52182a15ac04754ee04f2c7e947b4
Kaya-Sem <kayasemvc@gmail.com>
2024-04-27 15:15:51 +0200

    added styling

--- src/main/java/be/ugent/objprog/ugentopoly/logging/LogElement.java
+++ src/main/java/be/ugent/objprog/ugentopoly/logging/LogElement.java
@@ -1,5 +1,6 @@
 package be.ugent.objprog.ugentopoly.logging;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
@@ -7,13 +8,16 @@ import javafx.scene.text.FontWeight;
 
 
 public class LogElement extends HBox {
+    private static final int SPACING = 9;
 
     public LogElement(String playerName, String actionPerformed) {
         Label name = new Label(playerName);
-        name.setFont(Font.font("Arial", FontWeight.BOLD, 11));
+        name.setFont(Font.font("Arial", FontWeight.BOLD, 13));
         Label action = new Label(actionPerformed);
 
-        // TODO add styling
+        setAlignment(Pos.CENTER_LEFT);
+        setSpacing(SPACING);
+
         getChildren().addAll(name, action);
     }
 }
