7f42cc044d7846aceabe795d2dfe9490b34249a7
Kaya-Sem <kayasemvc@gmail.com>
2024-04-28 12:02:05 +0200

    refactor and formatting

--- src/main/java/be/ugent/objprog/ugentopoly/CurrentPlayerIndicator.java
+++ src/main/java/be/ugent/objprog/ugentopoly/CurrentPlayerIndicator.java
@@ -3,6 +3,11 @@ package be.ugent.objprog.ugentopoly;
 import be.ugent.objprog.ugentopoly.players.PlayerModel;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
@@ -13,25 +18,24 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
-// TODO add current balance
-
 public class CurrentPlayerIndicator extends VBox implements InvalidationListener {
     protected static final double SPACING = 20.0;
     protected static final double HEIGHT = 70.0;
     private final GameModel gameModel;
     private PlayerModel currentPlayer;
-    private String playerName;
-    private Image image;
-    private String balance;
+
+    private final StringProperty playerName;
+    private final ObjectProperty<Image> image;
+    private final StringProperty balance;
 
     public CurrentPlayerIndicator(GameModel gameModel) {
         this.gameModel = gameModel;
         this.gameModel.addListener(this);
         currentPlayer = gameModel.getCurrentPlayerMove();
 
-        playerName =currentPlayer.getPlayerName();
-        image = currentPlayer.getBadgeImage();
-        balance = String.valueOf(currentPlayer.getBalance());
+        playerName = new SimpleStringProperty(currentPlayer.getPlayerName());
+        image = new SimpleObjectProperty<>(currentPlayer.getBadgeImage());
+        balance = new SimpleStringProperty(String.valueOf(currentPlayer.getBalance()));
 
         Label text = new Label("Now Playing:");
 //        text.getStylesheets().add("medium-title"); TODO cannot load stylesheets?
@@ -47,42 +51,41 @@ public class CurrentPlayerIndicator extends VBox implements InvalidationListener
        setMaxHeight(HEIGHT);
     }
 
-
-
     private class PlayerInfo extends HBox{
-        protected static final double BADGESIZE = 70.0;
-        protected static final double DISTANCE = 20.0;
 
         private PlayerInfo() {
 
-            Label playerNameLabel = new Label(playerName);
+            Label playerNameLabel = new Label();
+            playerNameLabel.textProperty().bind(playerName);
             playerNameLabel.setAlignment(Pos.CENTER);
             playerNameLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20.0));
 
-            Label balanceLabel = new Label("€" + balance);
+            Label balanceLabel = new Label();
+            balanceLabel.textProperty().bind(Bindings.createStringBinding(
+                    () -> "€" + balance.get(),
+                    balance
+            ));
+
             balanceLabel.setFont(Font.font("Consolas", FontWeight.THIN, 12.0));
 
             VBox vBox = new VBox(playerNameLabel, balanceLabel );
             vBox.setAlignment(Pos.CENTER);
 
-            ImageView imageView = new ImageView(image);
-            imageView.setPreserveRatio(true);
-            imageView.setFitHeight(BADGESIZE);
-            imageView.setFitWidth(BADGESIZE);
+            ImageView imageView = new CustomImageView(HEIGHT, HEIGHT);
+            imageView.imageProperty().bind(image);
 
             getChildren().addAll(imageView, vBox);
             setAlignment(Pos.CENTER);
-            setSpacing(DISTANCE);
+            setSpacing(SPACING);
         }
     }
 
     @Override
     public void invalidated(Observable observable) {
-        GameModel model = (GameModel) observable;
-        currentPlayer = model.getCurrentPlayerMove();
-        playerName = currentPlayer.getPlayerName();
-        image = currentPlayer.getBadgeImage();
-        balance = String.valueOf(currentPlayer.getBalance());
+        currentPlayer = gameModel.getCurrentPlayerMove();
+        playerName.set(currentPlayer.getPlayerName());
+        image.set(currentPlayer.getBadgeImage());
+        balance.set(String.valueOf(currentPlayer.getBalance()));
     }
 
     public GameModel getGameModel() {
