665bd86dcf6b3c024779f15dc585d4508ec52803
Kaya-Sem <kayasemvc@gmail.com>
2024-04-28 13:21:28 +0200

    converted to enum, removed testcase.

--- src/main/java/be/ugent/objprog/ugentopoly/parsers/PropertyLoader.java
+++ src/main/java/be/ugent/objprog/ugentopoly/parsers/PropertyLoader.java
@@ -1,8 +1,10 @@
 package be.ugent.objprog.ugentopoly.parsers;
 
+import java.io.IOException;
 import java.util.Properties;
 
-public class PropertyLoader {
+public enum PropertyLoader {
+    ;
     private static final Properties properties;
 
     static {
@@ -10,21 +12,12 @@ public class PropertyLoader {
         try {
             properties.load(
                     PropertyLoader.class.getResourceAsStream("/be/ugent/objprog/ugentopoly/ugentopoly.properties"));
-        } catch (Exception e) {
-            System.err.println("Property file could not be loaded!");
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static String getLabel(String tileId) {
         return properties.getProperty(tileId);
-    }
-
-    private static void test() {
-        System.out.println(getLabel("tile.street01"));
-    }
-
-    public static void main(String[] args) {
-        test();
     }
 }
