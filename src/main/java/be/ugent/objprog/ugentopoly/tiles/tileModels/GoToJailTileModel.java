f642d5317687b86cf1eb7678f62f2ff0ef1cd4a2
Kaya-Sem <kayasemvc@gmail.com>
2024-04-29 19:08:00 +0200

    called gamecontrollers goToJail instead of implementing it in the method itself

--- src/main/java/be/ugent/objprog/ugentopoly/tiles/tileModels/GoToJailTileModel.java
+++ src/main/java/be/ugent/objprog/ugentopoly/tiles/tileModels/GoToJailTileModel.java
@@ -3,11 +3,8 @@ package be.ugent.objprog.ugentopoly.tiles.tileModels;
 import be.ugent.objprog.ugentopoly.CustomImage;
 import be.ugent.objprog.ugentopoly.DisplayCardController;
 import be.ugent.objprog.ugentopoly.GameModel;
-import be.ugent.objprog.ugentopoly.parsers.PropertyLoader;
-import be.ugent.objprog.ugentopoly.players.Pion;
 import be.ugent.objprog.ugentopoly.players.PlayerModel;
 import be.ugent.objprog.ugentopoly.tiles.tileCards.BasicVerticalCard;
-import javafx.scene.image.Image;
 
 import java.util.function.Consumer;
 
@@ -24,14 +21,7 @@ public class GoToJailTileModel extends TileModel{
     public Consumer<GameModel> getPlayerTileInteraction() {
         return gameModel -> {
             PlayerModel currentPlayer = gameModel.getCurrentPlayerMove();
-            Pion pion = currentPlayer.getPion();
-            TileModel[] tileModelList = gameModel.getTileModels();
-
-            tileModelList[pion.getPosition()].removePion(pion);
-            tileModelList[10].addPion(pion);
-
-            currentPlayer.setInJail(true);
-
+            gameModel.getGameController().moveCurrentPlayerToJail();
             gameModel.addLog(currentPlayer.getPlayerName(), "moet een nachtje in de Overpoort spenderen");
         };
     }
