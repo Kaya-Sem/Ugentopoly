397bf5d69740b6ae786810904298d4d274cfb603
Kaya-Sem <kayasemvc@gmail.com>
2024-04-30 20:26:06 +0200

    refactor and extraction

--- src/main/java/be/ugent/objprog/ugentopoly/Ugentopoly.java
+++ src/main/java/be/ugent/objprog/ugentopoly/Ugentopoly.java
@@ -1,9 +1,9 @@
 package be.ugent.objprog.ugentopoly;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
+import be.ugent.objprog.ugentopoly.gamecards.CardDeck;
 import be.ugent.objprog.ugentopoly.dice.DiceModel;
 import be.ugent.objprog.ugentopoly.dice.DiceRoller;
 import be.ugent.objprog.ugentopoly.factories.GameCardFactory;
@@ -14,11 +14,14 @@ import be.ugent.objprog.ugentopoly.parsers.XMLParser;
 import be.ugent.objprog.ugentopoly.players.PlayerCreatorStage;
 import be.ugent.objprog.ugentopoly.players.PlayerModel;
 import be.ugent.objprog.ugentopoly.playertabpane.PlayerTabPane;
+import be.ugent.objprog.ugentopoly.tiles.InitializedTilesObject;
+import be.ugent.objprog.ugentopoly.tiles.TileInitializer;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.ListView;
 
@@ -26,61 +29,43 @@ public class Ugentopoly extends Application {
     private static final String APPLICATIONNAME = "Ugentopoly";
     private static final double WINDOW_WIDTH = 1700.0;
     private static final double WINDOW_HEIGHT = 1200.0;
-    public final XMLParser parser = new XMLParser(); // TODO move to auxillary class
+    public final XMLParser parser = new XMLParser();
 
-    // HACK decouple
     @Override
     public void start(Stage primaryStage) {
 
         PlayerCreatorStage playerCreatorStage = new PlayerCreatorStage(parser.getStartingBalance());
         List<PlayerModel> players = playerCreatorStage.showAndWaitForPlayers();
 
-        // HACK create Auxillary object class
+        ComponentInitializer components = new ComponentInitializer(parser, players);
+        GameModel gameModel = components.getGameModel();
 
-        BoardModel boardModel = new BoardModel();
-        DisplayCardController cardController = new DisplayCardController(boardModel);
+        GameController gameController = new GameController(primaryStage, components.getGameModel(), components.getDiceModel());
+        gameModel.setGameController(gameController);
 
-        TileFactory factory = new TileFactory(parser.areaColors(), parser.getStartAmount(), cardController);
-        TileInitializer tileInitializer = new TileInitializer(parser, factory);
-        Map<String, Object[]> tileMap = tileInitializer.TileModelInitializer();
-        Object[] tileModelMap =  tileMap.get("models");
+        DiceRoller diceRoller = new DiceRoller(components.getDiceModel());
 
-        GameCardInitializer gameCardInitializer = new GameCardInitializer(parser, new GameCardFactory());
-
-        DiceModel diceModel = new DiceModel();
-
-        GameModel gameModel = new GameModel(
-                players,
-                tileModelMap,
-                gameCardInitializer.getChanceCards(),
-                gameCardInitializer.getChestCards(),
-                diceModel
-        );
-
-        GameController gameController = new GameController(primaryStage, gameModel, diceModel);
-        DiceRoller diceRoller = new DiceRoller(diceModel);
-
-        // todo create method for initializeing UI elements
-        CurrentPlayerIndicator playerIndicator = new CurrentPlayerIndicator(gameModel);
-
+        VBox verticalComponents = new VBox();
+         verticalComponents.setSpacing(10);
+         verticalComponents.setAlignment(Pos.CENTER);
         PlayerTabPane playerTabPane = new PlayerTabPane(gameModel.getPlayerModels());
+
+        ListView logListView = new ListView();
+        logListView.setItems(gameModel.getLogs());
+        logListView.setSelectionModel(null);
+
+         verticalComponents.getChildren().addAll(new CurrentPlayerIndicator(gameModel), playerTabPane, logListView, diceRoller);
 
         HBox horizontalLayout = new HBox();
         horizontalLayout.setAlignment(Pos.CENTER);
-        horizontalLayout.setSpacing(60);
 
-        ListView logListView = new ListView();
-        logListView.setMaxHeight(845);
-        logListView.setMaxWidth(600);
-        logListView.setItems(gameModel.getLogs());
-        logListView.setSelectionModel(null);
-        // TODO add custom cell factory to include badge
+        horizontalLayout.getChildren().addAll(
+                new Board(components.getBoardModel(),
+                          components.getInitializedTilesObject()),
+                          verticalComponents);
 
-        horizontalLayout.getChildren().addAll(logListView, new Board(boardModel,tileMap), playerTabPane);
+        StackPane root = new StackPane(horizontalLayout);
 
-        StackPane root = new StackPane(horizontalLayout, playerIndicator, diceRoller);
-
-        StackPane.setAlignment(playerIndicator, Pos.TOP_CENTER);
         StackPane.setAlignment(playerTabPane, Pos.CENTER_RIGHT);
         StackPane.setAlignment(diceRoller, Pos.BOTTOM_RIGHT);
 
@@ -88,8 +73,7 @@ public class Ugentopoly extends Application {
 
         Scene scene = new Scene(root, BoardModel.BOARD_SIZE, BoardModel.BOARD_SIZE);
 
-        scene.getStylesheets().add(Objects
-                .requireNonNull(getClass().getResource("/be/ugent/objprog/ugentopoly/styles.css")).toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("/be/ugent/objprog/ugentopoly/styles.css").toExternalForm());
 
         primaryStage.setScene(scene);
         primaryStage.show();
