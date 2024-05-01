cb09396ac145acea7f577fdaa44f2a2f07118ada
Kaya-Sem <kayasemvc@gmail.com>
2024-05-01 18:50:55 +0200

    changed size

--- src/main/java/be/ugent/objprog/ugentopoly/playertabpane/TileCell.java
+++ src/main/java/be/ugent/objprog/ugentopoly/playertabpane/TileCell.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 public class TileCell extends ListCell<TileModel> {
     private final Map<Class<?>, TileGraphicGenerator> generatorMap = new HashMap<>();
-    private static final double GRAPHICSIZE = 40;
+    private static final double GRAPHICSIZE = 33;
 
     public TileCell() {
         generatorMap.put(ColorTile.class, TileCell::generateColorView);
