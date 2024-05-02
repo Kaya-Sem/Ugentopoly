b3740a026adebfc66b2612897696105b6dde758b
Kaya-Sem <kayasemvc@gmail.com>
2024-05-02 21:17:37 +0200

    implemented logic to show owner

--- src/main/java/be/ugent/objprog/ugentopoly/tiles/tileCards/RailwayCard.java
+++ src/main/java/be/ugent/objprog/ugentopoly/tiles/tileCards/RailwayCard.java
@@ -1,47 +1,40 @@
 package be.ugent.objprog.ugentopoly.tiles.tileCards;
 
+import be.ugent.objprog.ugentopoly.CustomImageView;
+import be.ugent.objprog.ugentopoly.tiles.tileModels.RailwayTileModel;
 import be.ugent.objprog.ugentopoly.tiles.tileViews.TileView;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.TextAlignment;
 
 public class RailwayCard extends VerticalCard{
-    public RailwayCard(Image image, String text, String cost, String rent) {
-        Label textLabel = new Label(text);
-        Label costLabel = new Label(cost + "€");
+    public RailwayCard(RailwayTileModel model) {
+        Label nameLabel = new Label(model.getName());
+        Label ownerLabel = new Label();
+        Label costLabel = new Label("€" + model.getCost());
         costLabel.setTextAlignment(TextAlignment.LEFT);
 
-        textLabel.setFont(Font.font("Arial", FontWeight.BOLD, 15));
+        ownerLabel.textProperty().bind(model.ownerProperty());
+        ownerLabel.getStyleClass().add("owner-label");
 
-        VBox title = new VBox(textLabel, costLabel);
+        nameLabel.getStyleClass().add("bold-label-large");
+
+        VBox title = new VBox(nameLabel, costLabel);
         title.setAlignment(Pos.CENTER);
 
-        Label preText = new Label(" rent");
-        Label rentLabel = new Label(rent + "€");
-        rentLabel.setFont(Font.font("Arial", FontWeight.BOLD, 13));
+        Label rentLabel = new Label("rent €" + model.getRent());
+        rentLabel.getStyleClass().add("bold-label-medium");
 
-        HBox rentBox = new HBox(rentLabel, preText);
-        rentBox.setAlignment(Pos.CENTER);
+        ImageView imageView = new CustomImageView(TileView.LONG_SIDE, TileView.LONG_SIDE, model.getImage());
 
-
-        ImageView imageView = new ImageView(image);
-        imageView.setPreserveRatio(true);
-        imageView.setFitHeight(TileView.LONG_SIDE);
-        imageView.setFitWidth(TileView.LONG_SIDE);
-
-        VBox vBox = new VBox(imageView, title, rentBox);
+        VBox vBox = new VBox(imageView, title, ownerLabel, rentLabel);
         vBox.setAlignment(Pos.CENTER);
         vBox.setPrefHeight(Double.MAX_VALUE);
         vBox.setPrefWidth(Double.MAX_VALUE);
         vBox.setSpacing(10);
 
         getChildren().addAll(vBox);
-
     }
 }
