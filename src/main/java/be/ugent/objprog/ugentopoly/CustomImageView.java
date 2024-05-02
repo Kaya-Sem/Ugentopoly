dc1cf81b7da33b859234880dfd9789b65716c8e1
Kaya-Sem <kayasemvc@gmail.com>
2024-05-02 20:30:38 +0200

    added builder pattern with dropshadow

--- src/main/java/be/ugent/objprog/ugentopoly/CustomImageView.java
+++ src/main/java/be/ugent/objprog/ugentopoly/CustomImageView.java
@@ -1,12 +1,12 @@
 package be.ugent.objprog.ugentopoly;
 
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 
 public class CustomImageView extends ImageView {
-    public CustomImageView() {
-        setPreserveRatio(true);
-    }
+    protected static final double DROPSHADOWRADIUS = 10.0;
 
     public CustomImageView(double fitHeight, double fitWidth, Image image) {
         setImage(image);
@@ -15,6 +15,17 @@ public class CustomImageView extends ImageView {
 
     public CustomImageView(double fitSize) {
         applyCommonSettings(fitSize, fitSize);
+    }
+
+    // builder pattern
+    public CustomImageView addDropShadow() {
+        DropShadow shadow = new DropShadow();
+        shadow.setRadius(DROPSHADOWRADIUS);
+        shadow.setColor(Color.BLACK);
+
+        setEffect(shadow);
+
+        return this;
     }
 
     private void applyCommonSettings(double height, double width) {
