ee10cb550fd9922a66c8948e522e7c24085eaef6
Kaya-Sem <kayasemvc@gmail.com>
2024-05-02 21:21:40 +0200

    implemented logic to show owner

--- src/main/java/be/ugent/objprog/ugentopoly/tiles/tileCards/UtilityCard.java
+++ src/main/java/be/ugent/objprog/ugentopoly/tiles/tileCards/UtilityCard.java
@@ -1,29 +1,26 @@
 package be.ugent.objprog.ugentopoly.tiles.tileCards;
 
+import be.ugent.objprog.ugentopoly.CustomImageView;
+import be.ugent.objprog.ugentopoly.tiles.tileModels.UtilityTileModel;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 
 public class UtilityCard extends HorizontalCard {
     private static final double PADDING = 40;
 
+    public UtilityCard(UtilityTileModel model){
+        ImageView imageView = new CustomImageView(WIDTH - PADDING, HEIGHT - PADDING, model.getImage());
 
-    public UtilityCard(Image image, String cost){
+        Label amount = new Label("€" + model.getCost());
+        Label ownerLabel = new Label();
+        ownerLabel.textProperty().bind(model.ownerProperty());
 
+        ownerLabel.getStyleClass().add("owner-label");
+        amount.getStyleClass().add("bold-label-large");
 
-        ImageView imageView = new ImageView(image);
-        imageView.setPreserveRatio(true);
-        imageView.setFitWidth(WIDTH - PADDING);
-        imageView.setFitHeight(HEIGHT - PADDING);
-
-        Label amount = new Label( cost + "€");
-        amount.setFont(Font.font("Arial", FontWeight.BOLD, 14));
-
-        VBox vBox = new VBox( imageView, amount);
+        VBox vBox = new VBox(imageView, ownerLabel, amount);
         vBox.setAlignment(Pos.CENTER);
         vBox.setSpacing(10);
 
