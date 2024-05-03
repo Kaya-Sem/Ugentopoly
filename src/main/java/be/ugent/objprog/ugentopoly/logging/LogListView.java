213d1e4fba554894df6856a586c27331ca5ecd2c
Kaya-Sem <kayasemvc@gmail.com>
2024-05-03 16:14:38 +0200

    styling changes and bugfix!!!!

--- src/main/java/be/ugent/objprog/ugentopoly/logging/LogListView.java
+++ src/main/java/be/ugent/objprog/ugentopoly/logging/LogListView.java
@@ -5,13 +5,19 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.ListView;
 
 public class LogListView extends ListView {
+    protected static final int WIDTH = 300;
+    protected static final int HEIGHT = 200;
+
     public LogListView(ObservableList<LogElement> logs) {
         super(logs);
         setSelectionModel(null);
         logs.addListener(this::onChanged);
+        setMinWidth(WIDTH);
+        setMaxHeight(HEIGHT);
     }
 
     // Always scroll down to the most recently added element, so the user does not have to scroll manually.
+    // this is automatic in textfield, but would offer less control over elements
     private void onChanged(ListChangeListener.Change<? extends LogElement> change) {
     while (change.next()) {
         if (change.wasAdded()) {
